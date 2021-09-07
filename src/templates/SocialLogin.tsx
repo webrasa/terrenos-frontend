b4406fd461022700bc4a3ec513e96889a9594b99
Rémi Weng <ixartz@users.noreply.github.com>
2021-09-07 18:28:35 +0200

    Add Amplify Auth configuration

--- src/templates/SocialLogin.tsx
+++ src/templates/SocialLogin.tsx
@@ -1,66 +1,75 @@
-import Link from 'next/link';
+import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
+import { Auth } from 'aws-amplify';
 
 import { SocialButton } from '../button/SocialButton';
 import { FullCenterSection } from '../layout/FullCenterSection';
 
-const SocialLogin = () => (
-  <FullCenterSection title="Sign in to your account">
-    <button className="w-full" type="button">
-      <Link href="/dashboard">
-        <a>
-          <SocialButton
-            icon={
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-                <defs>
-                  <path
-                    id="a"
-                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
-                  />
-                </defs>
-                <clipPath id="b">
-                  <use xlinkHref="#a" overflow="visible" />
-                </clipPath>
-                <path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
-                <path
-                  clipPath="url(#b)"
-                  fill="#EA4335"
-                  d="M0 11l17 13 7-6.1L48 14V0H0z"
-                />
-                <path
-                  clipPath="url(#b)"
-                  fill="#34A853"
-                  d="M0 37l30-23 7.9 1L48 0v48H0z"
-                />
-                <path
-                  clipPath="url(#b)"
-                  fill="#4285F4"
-                  d="M48 48L17 24l-4-3 35-10z"
-                />
-              </svg>
-            }
-          >
-            Sign in with Google
-          </SocialButton>
-        </a>
-      </Link>
-    </button>
+const SocialLogin = () => {
+  const handleSignInGoogle = () => {
+    Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google });
+  };
 
-    <button className="w-full" type="button">
-      <SocialButton
-        icon={
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14222 14222">
-            <circle cx="7111" cy="7112" r="7111" fill="#1977f3" />
-            <path
-              d="M9879 9168l315-2056H8222V5778c0-562 275-1111 1159-1111h897V2917s-814-139-1592-139c-1624 0-2686 984-2686 2767v1567H4194v2056h1806v4969c362 57 733 86 1111 86s749-30 1111-86V9168z"
-              fill="#fff"
-            />
-          </svg>
-        }
-      >
-        Sign in with Facebook
-      </SocialButton>
-    </button>
-  </FullCenterSection>
-);
+  const handleSignInFacebook = () => {
+    Auth.federatedSignIn({
+      provider: CognitoHostedUIIdentityProvider.Facebook,
+    });
+  };
+
+  return (
+    <FullCenterSection title="Sign in to your account">
+      <button className="w-full" type="button" onClick={handleSignInGoogle}>
+        <SocialButton
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+              <defs>
+                <path
+                  id="a"
+                  d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                />
+              </defs>
+              <clipPath id="b">
+                <use xlinkHref="#a" overflow="visible" />
+              </clipPath>
+              <path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+              <path
+                clipPath="url(#b)"
+                fill="#EA4335"
+                d="M0 11l17 13 7-6.1L48 14V0H0z"
+              />
+              <path
+                clipPath="url(#b)"
+                fill="#34A853"
+                d="M0 37l30-23 7.9 1L48 0v48H0z"
+              />
+              <path
+                clipPath="url(#b)"
+                fill="#4285F4"
+                d="M48 48L17 24l-4-3 35-10z"
+              />
+            </svg>
+          }
+        >
+          Sign in with Google
+        </SocialButton>
+      </button>
+
+      <button className="w-full" type="button" onClick={handleSignInFacebook}>
+        <SocialButton
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14222 14222">
+              <circle cx="7111" cy="7112" r="7111" fill="#1977f3" />
+              <path
+                d="M9879 9168l315-2056H8222V5778c0-562 275-1111 1159-1111h897V2917s-814-139-1592-139c-1624 0-2686 984-2686 2767v1567H4194v2056h1806v4969c362 57 733 86 1111 86s749-30 1111-86V9168z"
+                fill="#fff"
+              />
+            </svg>
+          }
+        >
+          Sign in with Facebook
+        </SocialButton>
+      </button>
+    </FullCenterSection>
+  );
+};
 
 export { SocialLogin };
--- src/utils/AwsConfig.ts
+++ src/utils/AwsConfig.ts
@@ -1,5 +1,20 @@
 // TODO: Add configuration for authentication
 export const AwsConfig = {
+  Auth: {
+    mandatorySignIn: true,
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    identityPoolId: process.env.NEXT_PUBLIC_AWS_AUTH_IDENTITY_POOL_ID,
+    userPoolId: process.env.NEXT_PUBLIC_AWS_AUTH_USER_POOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_AWS_AUTH_APP_CLIENT_ID,
+    oauth: {
+      domain: process.env.NEXT_PUBLIC_AWS_AUTH_DOMAIN,
+      scope: ['profile', 'openid', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: process.env.NEXT_PUBLIC_AWS_AUTH_REDIRECT_SIGN_IN,
+      redirectSignOut: process.env.NEXT_PUBLIC_AWS_AUTH_REDIRECT_SIGN_OUT,
+      clientId: process.env.NEXT_PUBLIC_AWS_AUTH_APP_CLIENT_ID,
+      responseType: 'code',
+    },
+  },
   API: {
     endpoints: [
       {
