6e61d6036658cf6e70957c3d73cedb7b74fe97ea
Rémi Weng <ixartz@users.noreply.github.com>
2022-01-21 23:35:19 +0100

    Rename from Login to SignUp in signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,11 +2,11 @@ import { Meta } from '../layout/Meta';
 import { SignUpForm } from '../templates/SignUpForm';
 import { AppConfig } from '../utils/AppConfig';
 
-const Login = () => (
+const SignUp = () => (
   <div className="antialiased text-gray-900">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <SignUpForm />
   </div>
 );
 
-export default Login;
+export default SignUp;
