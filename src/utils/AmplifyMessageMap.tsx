6cd0fce0c4f035a41f75149886d9615582456e66
Rémi Weng <ixartz@users.noreply.github.com>
2022-02-03 21:38:56 +0100

    Add comment in utils/ folder for new functions

--- src/utils/AmplifyMessageMap.tsx
+++ src/utils/AmplifyMessageMap.tsx
@@ -1,3 +1,7 @@
+/**
+ * Map the amplify message error and output the desired message.
+ * @param err - Exception raised by Amplify.
+ */
 export const mapAmplifyMessage = (err: unknown) => {
   if (err instanceof Error) {
     if (/Username cannot be empty/i.test(err.message)) {
--- src/utils/Session.tsx
+++ src/utils/Session.tsx
@@ -1,6 +1,11 @@
+/**
+ * Retrieve data from the browser SessionStorage.
+ * @param key - The name of the key to retrieve the value of.
+ */
 export const getSessionItem = (key: string) => {
   const isBrowser = typeof window !== 'undefined';
 
+  // sessionStorage is only available in the browser
   if (isBrowser) {
     return sessionStorage.getItem(key) || '';
   }
