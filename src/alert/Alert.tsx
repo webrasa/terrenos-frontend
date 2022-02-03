6eb3d3d63cc9cc9282557cd45a4c2c2b18532694
Rémi Weng <ixartz@users.noreply.github.com>
2022-02-03 21:29:07 +0100

    Add comments for Alert component

--- src/alert/Alert.tsx
+++ src/alert/Alert.tsx
@@ -4,6 +4,12 @@ type IAlertProps = {
   text?: ReactNode;
 };
 
+/**
+ * Alert shows message as feedback to the users.
+ * @component
+ * @params props - Component props.
+ * @param props.text - Text message to display.
+ */
 const Alert = (props: IAlertProps) => (
   <div className="text-left p-4 text-sm text-red-700 bg-red-100 rounded-lg mb-2">
     {props.text}
