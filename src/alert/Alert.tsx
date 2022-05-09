fb7d759144c58a47455047239db07431d56b6ce4
Remi W <ixartz@users.noreply.github.com>
2022-05-09 22:29:16 +0200

    new comment

--- src/alert/Alert.tsx
+++ src/alert/Alert.tsx
@@ -5,7 +5,7 @@ type IAlertProps = {
 };
 
 /**
- * Alert shows message as feedback to the users.
+ * Alert shows message as feedback to the users. New comment
  * @component
  * @params props - Component props.
  * @param props.text - Text message to display.
