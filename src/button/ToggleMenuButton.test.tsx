a995827ff0c380b4d35c7a6f2be67c0207fcc42a
Rémi Weng <ixartz@users.noreply.github.com>
2022-06-11 15:06:46 +0200

    test: add test when the user click on toggleMenuButton

--- src/button/ToggleMenuButton.test.tsx
+++ src/button/ToggleMenuButton.test.tsx
@@ -1,15 +1,17 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { ToggleMenuButton } from './ToggleMenuButton';
 
-describe('TogglemenuButton', () => {
+describe('ToggleMenuButton', () => {
   describe('onClick props', () => {
-    it('should call the callback', () => {
+    it('should call the callback when the user click on the button', () => {
       const handler = jest.fn();
 
       render(<ToggleMenuButton onClick={handler} />);
+      const button = screen.getByRole('button');
+      fireEvent.click(button);
 
-      expect(handler).not.toHaveBeenCalled();
+      expect(handler).toHaveBeenCalled();
     });
   });
 });
