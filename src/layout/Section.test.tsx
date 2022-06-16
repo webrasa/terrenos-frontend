1d1a1f238fca9bc6e43ed638052738767d80dd7d
Rémi Weng <ixartz@users.noreply.github.com>
2022-06-16 02:29:33 +0200

    test: add test for Section component

---
+++ src/layout/Section.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+
+import { Section } from './Section';
+
+describe('Section', () => {
+  describe('Render method', () => {
+    it('should render children without props', async () => {
+      render(<Section>Children</Section>);
+
+      const children = screen.queryByText('Children');
+
+      expect(children).toBeInTheDocument();
+    });
+
+    it('should always render children even if the title is defined', () => {
+      render(<Section title="Random title">Children</Section>);
+
+      const children = screen.queryByText('Children');
+
+      expect(children).toBeInTheDocument();
+    });
+
+    it('should always render children even if the description is defined', () => {
+      render(<Section description="Random description">Children</Section>);
+
+      const children = screen.queryByText('Children');
+
+      expect(children).toBeInTheDocument();
+    });
+
+    it('should always render children even if the title and the description are both defined', () => {
+      render(
+        <Section title="Random title" description="Random description">
+          Children
+        </Section>
+      );
+
+      const children = screen.queryByText('Children');
+
+      expect(children).toBeInTheDocument();
+    });
+  });
+});
