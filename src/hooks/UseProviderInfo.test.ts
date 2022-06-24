d43eabb328f713284ba56a75e08f48de1b7d3c55
Rémi Weng <ixartz@users.noreply.github.com>
2022-06-24 02:54:14 +0200

    test: start adding test for use providerInfo

--- src/hooks/UseProviderInfo.test.ts
+++ src/hooks/UseProviderInfo.test.ts
@@ -1,4 +1,5 @@
 import { renderHook, waitFor } from '@testing-library/react';
+import assert from 'assert';
 
 import { AuthState, useProviderInfo } from './UseProviderInfo';
 
@@ -16,6 +17,30 @@ describe('useProviderInfo', () => {
       await waitFor(() => {
         expect(result.current.userInfo).toEqual(AuthState.UNAUTHENTICATED);
       });
+    });
+
+    it('should bypass authentication for local testing', async () => {
+      // Save the original process.env
+      const originalEnv = process.env;
+      process.env = {
+        ...originalEnv,
+        NEXT_PUBLIC_COGNITO_USER_ID_LOCAL: 'COGNITO_USER_ID_LOCAL',
+        NEXT_PUBLIC_COGNITO_USER_EMAIL_LOCAL: 'COGNITO_USER_EMAIL_LOCAL',
+      };
+
+      const { result } = renderHook(() => useProviderInfo());
+      const { userInfo } = result.current;
+
+      assert(
+        userInfo !== AuthState.AUTHENTICATING &&
+          userInfo !== AuthState.UNAUTHENTICATED,
+        'user should be authenticated in local environment when process.env are set up correctly'
+      );
+      expect(userInfo.id).toEqual('COGNITO_USER_ID_LOCAL');
+      expect(userInfo.email).toEqual('COGNITO_USER_EMAIL_LOCAL');
+
+      // Restore the original process.env
+      process.env = originalEnv;
     });
   });
 });
