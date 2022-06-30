7185bb5230c0b376e11b77128afbc144ab73e9ea
Rémi Weng <ixartz@users.noreply.github.com>
2022-06-30 22:29:34 +0200

    test: add test for ConfirmDialog component

---
+++ __mocks__/intersectionObserverMock.ts
@@ -0,0 +1,12 @@
+// Need this mock to avoid warning/error with IntersectionObserver: https://stackoverflow.com/questions/57008341/jest-testing-react-component-with-react-intersection-observer
+export const intersectionObserverMock = () => ({
+  observe: jest.fn(),
+
+  disconnect: jest.fn(),
+
+  unobserve: jest.fn(),
+});
+
+window.IntersectionObserver = jest
+  .fn()
+  .mockImplementation(intersectionObserverMock);
---
+++ src/dialog/ConfirmDialog.test.tsx
@@ -0,0 +1,47 @@
+import '__mocks__/intersectionObserverMock';
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { ConfirmDialog } from './ConfirmDialog';
+
+describe('ConfirmDialog', () => {
+  describe('Render method', () => {
+    it('should have the dialog in hidden status', () => {
+      render(
+        <ConfirmDialog
+          title="Random title"
+          description="Random description"
+          show={false}
+          handleCancel={() => {}}
+          button={'button'}
+        />
+      );
+
+      const title = screen.queryByText('Random title');
+      expect(title).not.toBeInTheDocument();
+    });
+
+    it('should able to trigger callback when the user click on cancel button', async () => {
+      const handleCancel = jest.fn();
+
+      render(
+        <ConfirmDialog
+          title="Random title"
+          description="Random description"
+          show={true}
+          handleCancel={handleCancel}
+          button={'button'}
+        />
+      );
+
+      const title = screen.queryByText('Random title');
+      expect(title).toBeInTheDocument();
+
+      const cancelButton = screen.getByText('Cancel');
+      await userEvent.click(cancelButton);
+
+      expect(handleCancel).toBeCalled();
+    });
+  });
+});
