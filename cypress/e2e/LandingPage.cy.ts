2adf5a05b9f047403d6473b1b3fba85e2d22e8f4
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-08 04:17:43 +0200

    test: add tests for landing page

--- cypress/e2e/LandingPage.cy.ts
+++ cypress/e2e/LandingPage.cy.ts
@@ -3,6 +3,18 @@ describe('Landing page', () => {
     it('should render the landing page correctly', () => {
       // Start from the index page
       cy.visit('/');
+
+      cy.findByRole('link', { name: 'Login' })
+        .should('exist')
+        .invoke('attr', 'href')
+        .should('include', '/login');
+
+      cy.findByRole('link', { name: 'Start Free Trial' })
+        .should('exist')
+        .invoke('attr', 'href')
+        .should('include', '/signup');
+
+      cy.findByText(/© Copyright.*All Rights Reserved./).should('exist');
     });
   });
 });
