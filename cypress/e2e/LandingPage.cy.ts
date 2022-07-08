1a211999776b7c3dc32bb9ad1fcc60cc4ba326e4
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-08 03:49:37 +0200

    test: fix parsing error with eslint and typescript-eslint parser

---
+++ cypress/e2e/LandingPage.cy.ts
@@ -0,0 +1,8 @@
+describe('Landing page', () => {
+  describe('Render', () => {
+    it('should render the landing page correctly', () => {
+      // Start from the index page
+      cy.visit('/');
+    });
+  });
+});
