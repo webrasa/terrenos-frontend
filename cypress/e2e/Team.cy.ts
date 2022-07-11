9b74602a43317909485ca60576ea8538d8172361
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-11 23:57:35 +0200

    test: add tests to verify if the team display is correctly edited

--- cypress/e2e/Team.cy.ts
+++ cypress/e2e/Team.cy.ts
@@ -1,21 +1,26 @@
 import { nanoid } from 'nanoid';
 
 describe('Todo', () => {
+  const teamName = nanoid();
+
+  beforeEach(() => {
+    cy.visit('/dashboard');
+
+    // Display team selection and choose "create new team" option
+    cy.findByTestId('team-selection').click();
+    cy.findByText('Create new team').click();
+
+    // Create a new team with random name
+    cy.get('#displayName').type(teamName);
+    cy.findByRole('button', { name: 'Create' }).click();
+
+    // In team, we also verify if it redirects to index dashboard
+    cy.url().should('include', '/dashboard');
+  });
+
   describe('CRUD operation', () => {
-    it('should', () => {
-      const teamName = nanoid();
-
-      cy.visit('/dashboard');
-
-      // Display team selection and choose "create new team" option
-      cy.findByTestId('team-selection').click();
-      cy.findByText('Create new team').click();
-
-      // Create a new team with random name
-      cy.get('#displayName').type(teamName);
-      cy.findByRole('button', { name: 'Create' }).click();
-
-      cy.url().should('include', '/dashboard');
+    it('should edit team display name', () => {
+      const editTeamName = `${teamName}EDITED`;
 
       // Verify the created team appears in the team selection
       cy.findByTestId('team-selection').click();
@@ -23,8 +28,30 @@ describe('Todo', () => {
 
       // Go to the settings page
       cy.findByRole('link', { name: 'Settings' }).click();
-      // In the settings, it should display correctly the team name
-      cy.findAllByTestId('setting-line').findByText(teamName).should('exist');
+      cy.findAllByTestId('setting-line')
+        .filter(':contains("Display name")')
+        .within(() => {
+          // In the settings, it should display correctly the team name
+          cy.findByText(teamName).should('exist');
+
+          // Show the change team name dialog
+          cy.findByRole('button', { name: 'Change' }).click();
+        });
+      cy.findByRole('dialog').get('#displayName').type(editTeamName);
+      cy.findByRole('button', { name: 'Save' }).click();
+
+      // Wait for dialog to be closed
+      cy.findByRole('dialog').should('not.exist');
+      // Verify the team display name is correctly edited
+      cy.findAllByTestId('setting-line')
+        .filter(':contains("Display name")')
+        .within(() => {
+          cy.findByText(editTeamName).should('exist');
+        });
+
+      // Verify the edited team display name appears in the team selection
+      cy.findByTestId('team-selection').click();
+      cy.findByRole('listbox').findByText(editTeamName).should('exist');
     });
   });
 });
