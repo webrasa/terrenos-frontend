ff7daeaf84c439f252f3738cc53cef89de3a345e
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-15 22:57:45 +0200

    test: revert change in AWSConfig and add comment in .env.development related to AWS Cognito

--- src/utils/AwsConfig.ts
+++ src/utils/AwsConfig.ts
@@ -5,6 +5,14 @@ export const AwsConfig = {
     identityPoolId: process.env.NEXT_PUBLIC_AWS_AUTH_IDENTITY_POOL_ID,
     userPoolId: process.env.NEXT_PUBLIC_AWS_AUTH_USER_POOL_ID,
     userPoolWebClientId: process.env.NEXT_PUBLIC_AWS_AUTH_APP_CLIENT_ID,
+    oauth: {
+      domain: process.env.NEXT_PUBLIC_AWS_AUTH_DOMAIN,
+      scope: ['profile', 'openid', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: process.env.NEXT_PUBLIC_AWS_AUTH_REDIRECT_SIGN_IN,
+      redirectSignOut: process.env.NEXT_PUBLIC_AWS_AUTH_REDIRECT_SIGN_OUT,
+      clientId: process.env.NEXT_PUBLIC_AWS_AUTH_APP_CLIENT_ID,
+      responseType: 'code',
+    },
   },
   API: {
     endpoints: [
