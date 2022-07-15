454c82b8aaf4c3c04a0beb92843a62105d7fc575
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-15 22:44:01 +0200

    test: add test for managing Stripe subscription, Stripe customer portal

--- cypress/e2e/Billing.cy.ts
+++ cypress/e2e/Billing.cy.ts
@@ -1,11 +1,28 @@
+import { nanoid } from 'nanoid';
+
+import { SubscriptionPlan } from '@/types/SubscriptionPlan';
+
 describe('Billing', () => {
   describe('Simulated endpoint', () => {
-    it('should call the API endpoint for creating a Stripe checkout session', () => {
+    beforeEach(() => {
       cy.visit('/dashboard');
+
+      // Display team selection and choose "create new team" option
+      cy.findByTestId('team-selection').click();
+      cy.findByText('Create new team').click();
+
+      // Create a new team with random name
+      cy.get('#displayName').type(nanoid());
+      cy.findByRole('button', { name: 'Create' }).click();
+
+      // Wait until the team has been created
+      cy.location('pathname').should('eq', '/dashboard/');
 
       // Go to the settings page
       cy.findByRole('link', { name: 'Settings' }).click();
+    });
 
+    it('should call the API endpoint for creating a Stripe checkout session', () => {
       // Go to the upgrade page
       cy.findByRole('link', { name: 'Upgrade Plan' }).click();
 
@@ -13,7 +30,7 @@ describe('Billing', () => {
       const createCheckoutSpy = cy.spy().as('create-checkout-spy');
       cy.intercept(
         'POST',
-        `${Cypress.env('API_URL')}/**/billing/create-checkout-session`,
+        `${Cypress.env('API_URL')}/*/billing/create-checkout-session`,
         (req) => {
           req.continue((res) => {
             createCheckoutSpy();
@@ -38,6 +55,40 @@ describe('Billing', () => {
         .filter(':contains("PRO")')
         .findByText('Choose This Plan')
         .should('not.be.disabled');
+    });
+
+    it('should simulate a redirection to the Stripe customer portal to manage subscription', () => {
+      // Intercept backend call to avoid Stripe call
+      cy.intercept('GET', `${Cypress.env('API_URL')}/team/*/settings`, {
+        statusCode: 200,
+        body: {
+          hasStripeCustomerId: true,
+          planId: SubscriptionPlan.PRO,
+        },
+      });
+      cy.intercept(
+        'POST',
+        `${Cypress.env('API_URL')}/*/billing/customer-portal`,
+        {
+          statusCode: 200,
+          body: {
+            url: 'RANDOM_REDIRECT_URL',
+          },
+        }
+      );
+
+      // Verify upgrade button is hidden for non-FREE user
+      cy.findByRole('link', { name: 'Upgrade Plan' }).should('not.exist');
+
+      // Open Stripe customer portal
+      cy.findByText('Manage Plan').click();
+
+      // Verify if it has successfully redirected
+      // Only in testing environment, it redirects to a random url
+      cy.location('pathname').should(
+        'eq',
+        '/dashboard/settings/RANDOM_REDIRECT_URL/'
+      );
     });
   });
 });
