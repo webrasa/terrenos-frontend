80ba5bb42bafa8b5a7e9fabb98471a00a1b862aa
Rémi Weng <ixartz@users.noreply.github.com>
2022-09-21 01:22:52 +0200

    test: add tests for findTeamInd method

--- src/utils/Auth.test.ts
+++ src/utils/Auth.test.ts
@@ -1,6 +1,6 @@
 import { MemberRole } from '@/types/IMember';
 
-import { requiredRoles } from './Auth';
+import { findTeamInd, requiredRoles } from './Auth';
 
 describe('Auth utils', () => {
   describe('Verify roles', () => {
@@ -8,7 +8,7 @@ describe('Auth utils', () => {
       expect(requiredRoles([], MemberRole.READ_ONLY)).toBeFalsy();
     });
 
-    it('should return false if it is not in the list', () => {
+    it("should return false if it isn't in the list", () => {
       expect(
         requiredRoles(
           [MemberRole.OWNER, MemberRole.ADMIN],
@@ -21,6 +21,64 @@ describe('Auth utils', () => {
       expect(
         requiredRoles([MemberRole.OWNER, MemberRole.ADMIN], MemberRole.ADMIN)
       ).toBeTruthy();
+    });
+  });
+
+  describe('findTeamInd function', () => {
+    it('should return 0 by default when the list is undefined', () => {
+      const ind = findTeamInd(undefined, 'RANDOM_ID');
+
+      expect(ind).toEqual(0);
+    });
+
+    it('should return -1 when the list is emptied', () => {
+      const ind = findTeamInd([], 'RANDOM_ID');
+
+      expect(ind).toEqual(-1);
+    });
+
+    it("should return -1 when the team ID isn't found", () => {
+      const ind = findTeamInd(
+        [
+          {
+            id: 'id_1',
+            displayName: 'display_name_1',
+          },
+          {
+            id: 'id_2',
+            displayName: 'display_name_2',
+          },
+          {
+            id: 'id_3',
+            displayName: 'display_name_3',
+          },
+        ],
+        'RANDOM_ID'
+      );
+
+      expect(ind).toEqual(-1);
+    });
+
+    it('should return the correct index giving the team ID', () => {
+      const ind = findTeamInd(
+        [
+          {
+            id: 'id_1',
+            displayName: 'display_name_1',
+          },
+          {
+            id: 'id_2',
+            displayName: 'display_name_2',
+          },
+          {
+            id: 'id_3',
+            displayName: 'display_name_3',
+          },
+        ],
+        'id_2'
+      );
+
+      expect(ind).toEqual(1);
     });
   });
 });
