c2d79e8687b4f1452d414241cb1a8797aa026658
Rémi Weng <ixartz@users.noreply.github.com>
2022-12-27 22:44:00 +0100

    refactor: remove legacyBehavior for Next.js link in MenuLink

--- src/avatar/MenuLink.tsx
+++ src/avatar/MenuLink.tsx
@@ -1,42 +1,31 @@
 import { Menu } from '@headlessui/react';
-import type { PropsOf } from '@headlessui/react/dist/types';
 import classNames from 'classnames';
 import type { LinkProps } from 'next/link';
 import Link from 'next/link';
-import type { ForwardedRef, ReactNode } from 'react';
-import { forwardRef } from 'react';
+import type { ReactNode } from 'react';
 
 type IMenuInternalLinkProps = {
   active?: boolean;
 };
 
 const MenuInternalLink = (
-  props: PropsOf<'a'> &
-    React.PropsWithChildren<LinkProps> &
-    IMenuInternalLinkProps,
-  ref: ForwardedRef<HTMLAnchorElement>
+  props: React.PropsWithChildren<LinkProps> & IMenuInternalLinkProps
 ) => {
   const { href, children, active, ...rest } = props;
 
   return (
-    <Link href={href} legacyBehavior>
-      <a
-        ref={ref}
-        className={classNames(
-          'flex w-full justify-between py-2 px-4 text-left text-sm font-semibold leading-5',
-          active ? 'bg-primary-500 text-white' : 'text-gray-700'
-        )}
-        {...rest}
-      >
-        {children}
-      </a>
+    <Link
+      href={href}
+      className={classNames(
+        'flex w-full justify-between py-2 px-4 text-left text-sm font-semibold leading-5',
+        active ? 'bg-primary-500 text-white' : 'text-gray-700'
+      )}
+      {...rest}
+    >
+      {children}
     </Link>
   );
 };
-
-// Instead of applying direct forwardRef on MenuInternalLink, we use an intermediate component for Ref.
-// This avoid an eslint error and the solution was provided by: https://github.com/jsx-eslint/eslint-plugin-react/issues/2269
-const MenuInternalLinkRef = forwardRef(MenuInternalLink);
 
 type IMenuLinkProps = {
   href: string;
@@ -53,9 +42,9 @@ type IMenuLinkProps = {
 const MenuLink = (props: IMenuLinkProps) => (
   <Menu.Item>
     {({ active }) => (
-      <MenuInternalLinkRef href={props.href} active={active}>
+      <MenuInternalLink href={props.href} active={active}>
         {props.children}
-      </MenuInternalLinkRef>
+      </MenuInternalLink>
     )}
   </Menu.Item>
 );
