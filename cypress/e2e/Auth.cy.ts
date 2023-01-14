20950aea404a47398e22fe19b1b8029ca01744af
Rémi Weng <ixartz@users.noreply.github.com>
2023-01-14 23:27:21 +0100

    test: add tests when the user manually go to dashboard after confirming the account

--- cypress/e2e/Auth.cy.ts
+++ cypress/e2e/Auth.cy.ts
@@ -2,7 +2,7 @@ import { interceptSignIn } from '../utils/Auth';
 
 describe('Auth', () => {
   describe('Basic authentication', () => {
-    it('should sign up a new user using email', () => {
+    it('should sign up a new user using email and automatically redirect to dashboard', () => {
       // Start from the index page
       cy.visit('/');
 
@@ -39,6 +39,48 @@ describe('Auth', () => {
 
       // Display the success page for email confirmation
       cy.findByText('Your email has been verified');
+
+      // Verify the url after clicking the link
+      cy.location('pathname').should('eq', '/dashboard/');
+      cy.findByTestId('message-state')
+        .findByRole('link', { name: 'Add Todo' })
+        .should('exist');
+    });
+
+    it.only('should sign up a new user using email and manually redirect to dashboard', () => {
+      // Start from the index page
+      cy.visit('/');
+
+      // Go to signup page
+      cy.findByText('Start Free Trial').click();
+
+      // Intercept AWS Cognito request
+      cy.intercept('POST', 'https://cognito-idp.us-east-1.amazonaws.com/', {
+        statusCode: 200,
+      });
+
+      // Fill the signup form
+      cy.get('#email').type('random@email.com');
+      cy.get('#password').type('RANDOM_PASSWORD_TEST');
+      cy.findByRole('button', { name: 'Sign up' })
+        .click()
+        .should(() => {
+          // Verify the session storage is correctly set
+          expect(sessionStorage.getItem('confirm-signup-email')).to.eq(
+            'random@email.com'
+          );
+        });
+
+      // Verify if it has successfully redirected to `confirm-signup` page
+      cy.location('pathname').should('eq', '/confirm-signup/');
+
+      // Fill the confirm-sign form
+      cy.get('#verificationCode').type('RANDOM_VERIFICATION_CODE');
+      cy.findByRole('button', { name: 'Confirm' }).click();
+
+      // Display the success page for email confirmation
+      cy.findByText('Your email has been verified');
+      cy.findByText('Go to dashboard').click();
 
       // Verify the url after clicking the link
       cy.location('pathname').should('eq', '/dashboard/');
