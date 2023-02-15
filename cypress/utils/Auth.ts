c3f2feac96fdcbc7f7ff7fcdc8b7a8f0dccd26ef
Rémi Weng <ixartz@users.noreply.github.com>
2023-02-15 16:43:23 +0100

    test: add test when the MFA is enabled

--- cypress/utils/Auth.ts
+++ cypress/utils/Auth.ts
@@ -23,20 +23,6 @@ export const interceptSignIn = (cy: Cypress.cy & CyEventEmitter) => {
       method: 'POST',
       url: 'https://cognito-idp.us-east-1.amazonaws.com/',
       headers: {
-        'x-amz-target':
-          'AWSCognitoIdentityProviderService.RespondToAuthChallenge',
-      },
-    },
-    {
-      statusCode: 200,
-      fixture: 'auth/CognitoAuthChallenge.json',
-    }
-  );
-  cy.intercept(
-    {
-      method: 'POST',
-      url: 'https://cognito-idp.us-east-1.amazonaws.com/',
-      headers: {
         'x-amz-target': 'AWSCognitoIdentityProviderService.GetUser',
       },
     },
@@ -46,6 +32,44 @@ export const interceptSignIn = (cy: Cypress.cy & CyEventEmitter) => {
         UserAttributes: [],
         Username: '8c27d077-fd80-4cda-9b49-b4745d10037d',
       },
+    }
+  );
+};
+
+export const interceptMfaSignIn = (cy: Cypress.cy & CyEventEmitter) => {
+  cy.intercept(
+    {
+      method: 'POST',
+      url: 'https://cognito-idp.us-east-1.amazonaws.com/',
+      headers: {
+        'x-amz-target':
+          'AWSCognitoIdentityProviderService.RespondToAuthChallenge',
+      },
+    },
+    {
+      statusCode: 200,
+      body: {
+        ChallengeName: 'SOFTWARE_TOKEN_MFA',
+      },
+    }
+  );
+};
+
+export const interceptSuccessfullySignIn = (
+  cy: Cypress.cy & CyEventEmitter
+) => {
+  cy.intercept(
+    {
+      method: 'POST',
+      url: 'https://cognito-idp.us-east-1.amazonaws.com/',
+      headers: {
+        'x-amz-target':
+          'AWSCognitoIdentityProviderService.RespondToAuthChallenge',
+      },
+    },
+    {
+      statusCode: 200,
+      fixture: 'auth/CognitoAuthChallenge.json',
     }
   );
 };
