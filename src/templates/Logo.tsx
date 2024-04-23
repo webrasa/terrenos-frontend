d4ec8e9ac6701dbdf7716fa510b6b8ee5095e8ad
iwanzor <iwanzor@gmail.com>
2024-04-23 22:52:27 +0200

    Revert "navbar changes"

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -1,12 +1,37 @@
+import { AppConfig } from '@/utils/AppConfig';
+
 type ILogoProps = {
   xl?: boolean;
+  hideText?: boolean;
 };
 
 const Logo = (props: ILogoProps) => {
   const size = props.xl ? '44' : '32';
+  const fontStyle = props.xl
+    ? 'font-semibold text-2xl'
+    : 'font-semibold text-xl';
+
   return (
-    <span className={'sm:5/12 inline-flex w-4/12 items-center md:w-8/12'}>
-      <img src="/assets/images/logo.png" alt="Terrenoss logo" />
+    <span className={`inline-flex items-center text-gray-900 ${fontStyle}`}>
+      <svg
+        className="mr-1 stroke-current text-primary-500"
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M0 0h24v24H0z" stroke="none" />
+        <rect x="3" y="12" width="6" height="8" rx="1" />
+        <rect x="9" y="8" width="6" height="12" rx="1" />
+        <rect x="15" y="4" width="6" height="16" rx="1" />
+        <path d="M4 20h14" />
+      </svg>
+
+      {!props.hideText && AppConfig.site_name}
     </span>
   );
 };
--- src/templates/Navbar.tsx
+++ src/templates/Navbar.tsx
@@ -1,29 +1,45 @@
 import Link from 'next/link';
 
+import { Background } from '@/background/Background';
+import { Button } from '@/button/Button';
 import { LandingSection } from '@/layouts/LandingSection';
 import { NavbarMenuCenter } from '@/navigation/NavbarMenuCenter';
 
 import { Logo } from './Logo';
 
 const Navbar = () => (
-  <div className="border-b border-gray-300">
-    <LandingSection yPadding="py-4">
+  <Background color="bg-primary-100">
+    <LandingSection yPadding="py-6">
       <NavbarMenuCenter
         logo={<Logo xl />}
         rightMenu={
           <>
-            <li className="rounded-1.5xl bg-primary-600 p-2 text-center font-medium text-white md:px-10">
-              <Link href="/profile">Add a property</Link>
+            <li>
+              <Link href="/signin">Login</Link>
+            </li>
+            <li>
+              <Link href="/signup">
+                <Button>Start Free Trial</Button>
+              </Link>
             </li>
           </>
         }
       >
         <li>
-          <Link href="/search">View listings</Link>
+          <Link href="/signin">Products</Link>
+        </li>
+        <li>
+          <Link href="/signin">Examples</Link>
+        </li>
+        <li>
+          <Link href="/signin">Blog</Link>
+        </li>
+        <li>
+          <Link href="/signin">Contact</Link>
         </li>
       </NavbarMenuCenter>
     </LandingSection>
-  </div>
+  </Background>
 );
 
 export { Navbar };
