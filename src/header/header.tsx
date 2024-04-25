7df2aa2d61b83d442c50620b8e03a392e8a17bf2
rikisoni <jelenaa.ristanovic@gmail.com>
2024-04-25 23:24:11 +0200

    header-component

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,18 +1,16 @@
-import ControllableStates from "./search";
 import Image from "next/image";
+import AutoComplete from "./search";
 
 const Header = () => {
   return (
     <div className="container mx-auto h-96 relative">
       {/* Header content */}
-      <div className="flex md:flex-row h-auto relative">
-        <div
-          className="top-8 left-5 md:top-36 md:left-10 md:h-40 w-1/2 relative md:z-10"
-        >
-          <h1 className="text-2xl relative md:text-6xl font-semibold text-black bottom-4">
+      <div className="md:flex md:flex-row h-auto relative">
+        <div className="top-8 left-5 md:top-36 md:left-10 md:h-40 w-full md:w-1/2 relative z-10">
+          <h1 className="text-3xl relative md:text-6xl font-semibold text-black bottom-4">
             Land your dream
           </h1>
-          <ControllableStates />
+          <AutoComplete />
         </div>
 
         {/* Background image */}
@@ -27,19 +25,19 @@ const Header = () => {
         ></div>
 
         {/* Content on the right */}
-        <div className="relative h-96 md:w-3/4">
+        <div className="relative h-96 w-1/4 md:w-3/4">
           <div className="relative w-96 md:w-auto inset-0 md:flex-row justify-center items-center z-10">
             <Image
-              className="absolute top-40 right-40 md:right-16 md:w-72 md:h-44 transform rotate-1"
+              className="absolute top-12 md:top-40 left-18 md:right-40 md:w-72 md:h-44 transform rotate-1"
               src="/assets/images/placeHeader.png"
               width={500}
               height={500}
               alt="place"
             />
           </div>
-          <div className="relative inset-0 md:left-24 top-44 md:flex justify-center z-10">
+          <div className="relative inset-0 md:left-80 top-12 md:top-44 md:flex justify-center z-10">
             <Image
-              className="w-11 h-14"
+              className="absolute left-32 w-14 h-18 md:w-11 md:h-14"
               src="/assets/images/locationHeader.png"
               width={500}
               height={500}
--- src/header/search.tsx
+++ src/header/search.tsx
@@ -1,72 +1,118 @@
-import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete, { autocompleteClasses } from "@mui/material/Autocomplete";
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
 
-import SearchIcon from "@mui/icons-material/Search";
+const people = [
+  { id: 1, name: "Wade Cooper" },
+  { id: 2, name: "Arlene Mccoy" },
+  { id: 3, name: "Devon Webb" },
+  { id: 4, name: "Tom Cook" },
+  { id: 5, name: "Tanya Fox" },
+  { id: 6, name: "Hellen Schmidt" },
+];
 
-const options = ["Option 1", "Option 2"];
+export default function AutoComplete() {
+  const [selected, setSelected] = useState(people[0]);
+  const [query, setQuery] = useState("");
 
-export default function ControllableStates() {
-  const [value, setValue] = React.useState(null);
-  const [inputValue, setInputValue] = React.useState("");
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) =>
+          person.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
-    <div>
-      <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={options}
-        popupIcon={
-          <SearchIcon
-            style={{
-              fill: "white",
-              backgroundColor: "#16a34a",
-              borderRadius: "20px",
-              width: "28px",
-              height: "28px",
-            }}
-          />
-        }
-        sx={{
-          width: 645,
-          [`& .${autocompleteClasses.popupIndicator}`]: {
-            transform: "none",
-          },
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-          borderRadius: "8px",
-          "& .MuiInputBase-root": {
-            // Style for input label
-            fontSize: "20px", // Font size
-            fontWeight: 600, // Font weight
-            color: "#919191",
-          },
-          ".MuiAutocomplete-inputFocused": {
-           maxWidth: "90%",
-          },
-        "@media (max-width: 767px)":{
-          width:"357px"
-        }
-        }}
-        renderInput={(params) => (
-          <TextField
-            sx={{
-              ".MuiOutlinedInput-notchedOutline": {
-                border: "none",
-              }
-            }}
-            {...params}
-            InputLabelProps={{ shrink: true }}
-            placeholder="Enter a country, town or city"
-          />
-        )}
-      />
+    <div className="top-16 w-96 md:w-full">
+      <Combobox value={selected} onChange={setSelected}>
+        <div className="relative">
+          <div className="relative w-full h-14 cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-10 text-gray-900 focus:ring-0 bg-white"
+              displayValue={(person) => person.name}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center m-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="#009F52"
+                className="w-12 h-12"
+              >
+                <path d="M8.25 10.875a2.625 2.625 0 1 1 5.25 0 2.625 2.625 0 0 1-5.25 0Z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.125 4.5a4.125 4.125 0 1 0 2.338 7.524l2.007 2.006a.75.75 0 1 0 1.06-1.06l-2.006-2.007a4.125 4.125 0 0 0-3.399-6.463Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+              {filteredPeople.length === 0 && query !== "" ? (
+                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+                filteredPeople.map((person) => (
+                  <Combobox.Option
+                    key={person.id}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? "bg-primary-600 text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={person}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {person.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? "text-white" : "text-teal-600"
+                            }`}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="currentColor"
+                              className="w-6 h-6"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m4.5 12.75 6 6 9-13.5"
+                              />
+                            </svg>
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
     </div>
   );
 }
