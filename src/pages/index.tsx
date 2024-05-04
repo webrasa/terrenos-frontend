6154846518cb087805b2538ffd13fbbcccbaca64
rikisoni <jelenaa.ristanovic@gmail.com>
2024-05-04 19:51:51 +0200

    search-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { PropertyCard } from '@/card/Card';
 import CardSlider from '@/card-slider/CardSlider';
+import Filter from '@/filter/filter';
 import { Header } from '@/header/header';
 import { Meta } from '@/layouts/Meta';
 import { Footer } from '@/templates/Footer';
@@ -53,6 +54,7 @@ const Index = () => {
           );
         })}
       </CardSlider>
+      <Filter />
       <Footer />
     </div>
   );
